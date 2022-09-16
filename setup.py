f367ea66be5dc9f917d828fde1db697e7320b5cd
tung-nd <ductungnguyen1997@gmail.com>
2022-09-16 05:00:36 +0000

    remove unncessary files

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="climate_pretraining",
-    packages=find_packages(),
-    install_requires=[],
-    version="0.0.1",
-    author="",
-)
