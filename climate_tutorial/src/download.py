6d135f01b776159be685feee3c42350c2db4d73c
Shashank Goel <shashankgoel@Shashanks-MacBook-Air.local>
2022-09-20 16:48:02 -0700

    merged sources

---
+++ climate_tutorial/src/download.py
@@ -0,0 +1,96 @@
+import os
+import cdsapi
+import argparse
+import subprocess
+
+months = [str(i).rjust(2, "0") for i in range(1, 13)]
+days = [str(i).rjust(2, "0") for i in range(1, 32)]
+times = [str(i).rjust(2, "0") + ":00" for i in range(0, 24)]
+
+def _download_copernicus(root, variable, year, pressure):
+    dataset = "era5"
+    path = os.path.join(root, dataset, variable, f"{variable}_{year}_0.25deg.nc")
+    print(f"Downloading {dataset} {variable} data for year {year} from copernicus")
+    if(os.path.exists(path)):
+        return
+    os.makedirs(os.path.dirname(path), exist_ok = True)
+
+    download_args = {
+        "product_type": "reanalysis",
+        "format": "netcdf",
+        "variable": variable,
+        "year": str(year),
+        "month": months,
+        "day": days,
+        "time": times,
+    }
+
+    client = cdsapi.Client()
+
+    if not pressure:
+        client.retrieve(
+            "reanalysis-era5-single-levels",
+            download_args,
+            path,
+        )
+    else:
+        download_args["pressure_level"] = [1000, 850, 500, 50]
+        client.retrieve(
+            "reanalysis-era5-pressure-levels",
+            download_args,
+            path,
+        )
+
+def _download_weatherbench(root, dataset, variable, resolution):
+    path = os.path.join(root, dataset, variable, f"{variable}_{resolution}deg")
+    print(f"Downloading {dataset} {variable} data for {resolution} resolution from weatherbench")
+    if(os.path.exists(path)):
+        return
+    os.makedirs(os.path.dirname(path), exist_ok = True)
+
+    if(dataset == "era5"):
+        url = (
+            "https://dataserv.ub.tum.de/s/m1524895"
+            "/download?path=%2F{resolution}deg%2F{variable}&files={variable}_{resolution}deg.zip"
+        ).format(resolution = resolution, variable = variable)
+    elif(dataset == "cmip6"):
+        url = (
+            "https://dataserv.ub.tum.de/s/m1524895"
+            "/download?path=%2FCMIP%2FMPI-ESM%2F{resolution}deg%2F{variable}&files={variable}_{resolution}deg.zip"
+        ).format(resolution = resolution, variable = variable)
+    
+    print(path)
+    subprocess.check_call(["wget", "--no-check-certificate", url, "-O", path + ".zip"])
+    subprocess.check_call(["unzip", path + ".zip", "-d", path])
+
+def download(source, **kwargs):
+    if(kwargs["root"] is None):
+        kwargs["root"] = f"~/.climate_tutorial/data/{source}/"
+
+    if(source == "copernicus"):
+        _download_copernicus(**kwargs)
+    elif(source == "weatherbench"):
+        _download_weatherbench(**kwargs)
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers(dest = "source")
+
+    subparser = subparsers.add_parser("copernicus")
+    subparser.add_argument("--root", type = str, default = None)
+    subparser.add_argument("--variable", type = str, required = True)
+    subparser.add_argument("--year", type = int, required = True)
+    subparser.add_argument("--pressure", action = "store_true", default = False)
+
+    subparser = subparsers.add_parser("weatherbench")
+    subparser.add_argument("--root", type = str, default = None)
+    subparser.add_argument("--variable", type = str, required = True)
+    subparser.add_argument("--dataset", type = str, choices = ["era5", "cmip6"], required = True)
+    subparser.add_argument("--resolution", type = str, default = "1.40625")
+
+    args = parser.parse_args()
+    download(**vars(args))
+
+if(__name__ == "__main__"):
+    main()
