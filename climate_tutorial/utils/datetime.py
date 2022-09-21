4eab198f2e10034ee7cd1adb98239d90163a4d17
Shashank Goel <shashankgoel@Shashanks-MacBook-Air.local>
2022-09-20 19:24:47 -0700

    minor fixes

--- climate_tutorial/utils/datetime.py
+++ climate_tutorial/utils/datetime.py
@@ -1,11 +1,21 @@
-class Year:
-    def __init__(self, year):
-        self.year = year
+Year = int
 
 class Days:
-    def __init__(self, days):
-        self.days = days
+    def __init__(self, value):
+        self.value = value
+
+    def days(self):
+        return self.value
+
+    def hours(self):
+        return self.value * 24
 
 class Hours:
-    def __init__(self, hours):
-        self.hours = hours
+    def __init__(self, value):
+        self.value = value
+    
+    def days(self):
+        return self.value // 24
+
+    def hours(self):
+        return self.value
