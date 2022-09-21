e01e6627335b1410cd97d5a96e4ee7140dc197ec
Shashank Goel <shashankgoel@Shashanks-MacBook-Air.local>
2022-09-20 18:36:01 -0700

    minor fixes

--- climate_tutorial/utils/utils.py
+++ climate_tutorial/utils/utils.py
@@ -1,8 +1,9 @@
 import os
 import xarray as xr
+from IPython import display
 
 def load_dataset(dir):
     return xr.open_mfdataset(os.path.join(dir, "*.nc"))
 
 def view(dataset):
-    print(dataset.t2m)
+    display(dataset.t2m)
