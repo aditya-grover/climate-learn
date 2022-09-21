0692cfa7aacf845d66d15ce6eae9c5d011d0f8ba
Shashank Goel <shashankgoel@Shashanks-MacBook-Air.local>
2022-09-20 18:25:18 -0700

    minor changes

--- climate_tutorial/data/__init__.py
+++ climate_tutorial/data/__init__.py
@@ -1,3 +1,3 @@
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# import os
+# import sys
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
