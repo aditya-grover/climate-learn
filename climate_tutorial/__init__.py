4a86f79d7afffa2e19b3f43f119187e1b4add9e6
Shashank Goel <shashankgoel@Shashanks-MacBook-Air.local>
2022-09-20 18:24:09 -0700

    directory changes

--- climate_tutorial/__init__.py
+++ climate_tutorial/__init__.py
@@ -1,1 +1,1 @@
-from .src import *
+# from .src import *
--- climate_tutorial/src/__init__.py
+++ climate_tutorial/src/__init__.py
@@ -1,7 +1,5 @@
 import os
 import sys
-from os.path import abspath, dirname
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-sys.path.append(dirname(dirname(abspath(__file__))))
-
-from .utils import *
+# from .utils import *
