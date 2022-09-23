aef4726f23fa3adb69ac80734c9e43745bedc09b
Shashank Goel <shashankgoel@Shashanks-Air.lan>
2022-09-23 12:02:53 -0700

    minor fixes

--- climate_tutorial/training/train.py
+++ climate_tutorial/training/train.py
@@ -7,18 +7,19 @@ logging.getLogger("lightning").setLevel(logging.ERROR)
 logging.getLogger("pytorch_lightning").setLevel(logging.ERROR)
 
 class Trainer:
-    checkpoint_callback = ModelCheckpoint(save_last = True, verbose = False, filename = "epoch_{epoch:03d}", auto_insert_metric_name = False)
-    summary_callback = RichModelSummary(max_depth = -1)
-    progress_callback = RichProgressBar()
-
     def __init__(self, seed = 0, accelerator = "gpu", precision = 16, max_epochs = 4, logger = False):
         seed_everything(seed)
+
+        checkpoint_callback = ModelCheckpoint(save_last = True, verbose = False, filename = "epoch_{epoch:03d}", auto_insert_metric_name = False)
+        summary_callback = RichModelSummary(max_depth = -1)
+        progress_callback = RichProgressBar()
+
         self.trainer = LitTrainer(
             logger = logger,
             accelerator = accelerator,
             precision = precision,
             max_epochs = max_epochs,
-            callbacks = [Trainer.checkpoint_callback, Trainer.summary_callback, Trainer.progress_callback]
+            callbacks = [checkpoint_callback, summary_callback, progress_callback]
         )
 
     def fit(self, model_module, data_module):
