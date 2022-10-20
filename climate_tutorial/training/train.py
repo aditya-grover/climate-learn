8367c1dc60809d002e3cb570a4bad30a3a659f62
Seongbin Park <shannonsbpark@gmail.com>
2022-10-20 00:29:58 -0700

    Changes to trainer
    Added validate method
    Added devices param defaulted to 1

--- climate_tutorial/training/train.py
+++ climate_tutorial/training/train.py
@@ -7,7 +7,7 @@ logging.getLogger("lightning").setLevel(logging.ERROR)
 logging.getLogger("pytorch_lightning").setLevel(logging.ERROR)
 
 class Trainer:
-    def __init__(self, seed = 0, accelerator = "gpu", precision = 16, max_epochs = 4, logger = False):
+    def __init__(self, seed = 0, accelerator = "gpu", precision = 16, max_epochs = 4, logger = False, devices=1):
         seed_everything(seed)
 
         checkpoint_callback = ModelCheckpoint(save_last = True, verbose = False, filename = "epoch_{epoch:03d}", auto_insert_metric_name = False)
@@ -19,11 +19,16 @@ class Trainer:
             accelerator = accelerator,
             precision = precision,
             max_epochs = max_epochs,
+            devices = devices,
             callbacks = [checkpoint_callback, summary_callback, progress_callback]
         )
 
     def fit(self, model_module, data_module):
         self.trainer.fit(model_module, data_module)
+    
+    
+    def validate(self, model_module, data_module):
+        self.trainer.validate(model_module, data_module)
     
     def test(self, model_module, data_module):
         self.trainer.test(model_module, data_module)
