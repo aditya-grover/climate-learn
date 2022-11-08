4ba1c8fb0853b59c88655c74e2cb02b1755e6d3c
Siddharth Nandy <siddharthnandy@g.ucla.edu>
2022-11-08 10:44:41 -0800

    rollout() and forward() merge from climate_uncertainty

--- climate_tutorial/models/components/resnet.py
+++ climate_tutorial/models/components/resnet.py
@@ -1,6 +1,7 @@
 from math import log
 import torch
 from torch import nn
+from torch.distributions.normal import Normal
 from .cnn_blocks import PeriodicConv2D, ResidualBlock, Upsample
 
 # Large based on https://github.com/labmlai/annotated_deep_learning_paper_implementations/blob/master/labml_nn/diffusion/ddpm/unet.py
@@ -102,21 +103,60 @@ class ResNet(nn.Module):
     def forward(self, x: torch.Tensor, y: torch.Tensor, out_variables, metric, lat):
         # B, C, H, W
         pred = self.predict(x)
-        return [m(pred, y, out_variables, lat) for m in metric], x
+        if not self.prob_type:
+            return [m(pred, y, out_variables, lat) for m in metric], x
+        else:
+            return metric(pred, y, out_variables, lat), x
 
-    def rollout(self, x, y, clim, variables, out_variables, steps, metric, transform, lat, log_steps, log_days):
-        if steps > 1:
-            assert len(variables) == len(out_variables)
+    def rollout(self, x: torch.Tensor, y: torch.Tensor, clim, variables, out_variables, steps, metric, transform, lat, log_steps, log_days, mean_transform, std_transform, lat, log_day):
+        """
+        Notes from climate_uncertainty repo merge
+        Shared function params before merge:
+            x, y, clim, variables, out_variables, metric
+        Unique function params for climate_tutorial before merge:
+            steps, transform, lat, log_steps, log_days
+        Unique function params for climate_uncertainty before merge:
+            mean_transform, std_transform, lat, log_day
+        """
+        if self.prob_type:
+            # x: B, C, H, W
+            b = x.shape[0]
 
-        preds = []
-        for _ in range(steps):
-            x = self.predict(x)
-            preds.append(x)
-        preds = torch.stack(preds, dim=1)
-        if len(y.shape) == 4:
-            y = y.unsqueeze(1)
+            if self.prob_type == 'mcdropout':
+                x = x.unsqueeze(1).repeat(1, self.n_samples, 1, 1, 1, 1).flatten(0, 1) # B x n_samples, C, H, W
 
-        return [m(preds, y, clim, transform, out_variables, lat, log_steps, log_days) for m in metric], preds
+            pred = self.predict(x) # Normal if parametric else Tensor
+
+            if self.prob_type == 'mcdropout':
+                pred = mean_transform(pred)
+                pred = pred.unflatten(dim=0, sizes=(b, self.n_samples)) # B, n_samples, C, H, W
+                mean = torch.mean(pred, dim=1)
+                std = torch.std(pred, dim=1)
+                pred = Normal(mean, std)
+            elif self.prob_type == 'parametric':
+                mean, std = pred.loc, pred.scale
+                mean = mean_transform(mean)
+                std = std_transform(std)
+                pred = Normal(mean, std)
+            else:
+                pred = mean_transform(pred)
+
+            y = mean_transform(y)
+
+            return [m(pred, y, clim, out_variables, lat, log_day) for m in metric], pred
+        else:
+            if steps > 1:
+                assert len(variables) == len(out_variables)
+
+            preds = []
+            for _ in range(steps):
+                x = self.predict(x)
+                preds.append(x)
+            preds = torch.stack(preds, dim=1)
+            if len(y.shape) == 4:
+                y = y.unsqueeze(1)
+
+            return [m(preds, y, clim, transform, out_variables, lat, log_steps, log_days) for m in metric], preds
 
     def upsample(self, x, y, out_vars, transform, metric):
         with torch.no_grad():
@@ -124,7 +164,9 @@ class ResNet(nn.Module):
         return [m(pred, y, transform, out_vars) for m in metric], pred
 
 
-# model = ResNet(in_channels=1, out_channels=1, upsampling=2).cuda()
-# x = torch.randn((64, 1, 32, 64)).cuda()
+# # model = ResNet(in_channels=1, out_channels=1, upsampling=2).cuda()
+# # x = torch.randn((64, 1, 32, 64)).cuda()
+# model = ResNet(history=3, in_channels=43, out_channels=3, prob_type='parametric').cuda()
+# x = torch.randn((2, 3, 43, 32, 64)).cuda()
 # y = model.predict(x)
 # print (y.shape)
