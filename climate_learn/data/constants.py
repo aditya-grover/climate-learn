0778f6e9d60ced79b744b165c2118d3a8701afc2
Seongbin Park <shannonsbpark@gmail.com>
2022-11-13 23:53:33 -0800

    moved support for download and regrid for cmip 6 to clean branch

---
+++ climate_learn/data/constants.py
@@ -0,0 +1,8 @@
+NAME_TO_CMIP = {
+    "geopotential": "zg",
+    "u_component_of_wind": "ua",
+    "v_component_of_wind": "va",
+    "temperature": "ta",
+    "relative_humidity": "r",
+    "specific_humidity": "hus",
+}
