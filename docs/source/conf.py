1721a05cdadbb8fda1ca5bd68e1c73997238f0a3
Jason Jewik <jason.jewik@gmail.com>
2022-12-06 15:56:34 -0800

    Updated config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,6 +2,7 @@
 import sphinx_rtd_theme
 import os
 import sys
+import climate_learn
 
 # Point ReadTheDocs to the directory
 sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
@@ -34,12 +35,19 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+# How to represents typehints
+autodoc_typehints = 'signature'
+
+# If true, the current module name will be prepended to all description
+# unit titles (such as .. function::).
+add_module_names = False
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
 
 html_theme_options = {
-    
+    'collapse_navigation': False
 }
 
 # -- Options for EPUB output
