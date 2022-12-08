c9a3c843742d61cde343a71dd91162108ecadc13
Siddharth Nandy <siddharthnandy@g.ucla.edu>
2022-12-08 13:57:40 -0800

    apply denormalization to lr and persistence predictions

--- climate_learn/models/modules/forecast.py
+++ climate_learn/models/modules/forecast.py
@@ -220,7 +220,7 @@ class ForecastLitModule(LightningModule):
 
         # rmse for persistence baseline
         pers_pred = x # B, 1, C, H, W
-        baseline_rmse = lat_weighted_rmse(pers_pred, y, out_variables, transform_pred=False, transform=self.denormalization, lat=self.lat, log_steps=steps, log_days=days)
+        baseline_rmse = lat_weighted_rmse(pers_pred, y, out_variables, transform_pred=True, transform=self.denormalization, lat=self.lat, log_steps=steps, log_days=days)
         for var in baseline_rmse.keys():
             self.log(
                 "test_persistence_baseline/" + var,
@@ -234,8 +234,8 @@ class ForecastLitModule(LightningModule):
         # rmse for linear regression baseline
         lr_pred = self.lr_baseline.predict(x.cpu().reshape((x.shape[0], -1))).reshape(y.shape)
         lr_pred = lr_pred[:, np.newaxis, :, :, :] # B, 1, C, H, W
-        lr_pred = torch.from_numpy(lr_pred).float().cuda()
-        baseline_rmse = lat_weighted_rmse(lr_pred, y, out_variables, transform_pred=False, transform=self.denormalization, lat=self.lat, log_steps=steps, log_days=days)
+        lr_pred = torch.from_numpy(lr_pred).float().to(y.device)
+        baseline_rmse = lat_weighted_rmse(lr_pred, y, out_variables, transform_pred=True, transform=self.denormalization, lat=self.lat, log_steps=steps, log_days=days)
         for var in baseline_rmse.keys():
             self.log(
                 "test_ridge_regression_baseline/" + var,
@@ -278,7 +278,7 @@ class ForecastLitModule(LightningModule):
 
         return {"optimizer": optimizer, "lr_scheduler": lr_scheduler}
 
-    def fit_lin_reg_baseline(self, train_dataset, reg_hparam=1.0):
+    def fit_lin_reg_baseline(self, train_dataset, reg_hparam=0.0):
         X_train = train_dataset.inp_data.reshape(train_dataset.inp_data.shape[0], -1)
         y_train = train_dataset.out_data.reshape(train_dataset.out_data.shape[0], -1)
         self.lr_baseline = Ridge(alpha=reg_hparam)
