b267181a14213e0977bf94863b83a23dbccf4d7e
Jason Jewik <jason.jewik@gmail.com>
2022-12-12 20:35:14 -0800

    Added nbsphinx extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinx_rtd_theme'
+    'sphinx_rtd_theme',
+    'nbsphinx'
 ]
 
 intersphinx_mapping = {
