12f26ef124d2ac44c2de8ff636fb8beff0d4e4d4
Prakhar Sharma <prakhar6sharma@gmail.com>
2023-02-10 00:26:34 -0800

    Formatted tests using black

--- tests/data/era5_dataset_test.py
+++ tests/data/era5_dataset_test.py
@@ -4,16 +4,24 @@ from climate_learn.data.modules import ERA5
 class TestERA5DatasetInstantiation:
     def test_era5(self):
         ERA5(
-            root_dir = '/data0/datasets/weatherbench/data/weatherbench/era5/5.625deg/',
-            root_highres_dir = None,
-            variables = ['2m_temperature', '10m_u_component_of_wind', '10m_v_component_of_wind'],
+            root_dir="/data0/datasets/weatherbench/data/weatherbench/era5/5.625deg/",
+            root_highres_dir=None,
+            variables=[
+                "2m_temperature",
+                "10m_u_component_of_wind",
+                "10m_v_component_of_wind",
+            ],
             years=range(2000, 2014),
         )
 
     def test_era5_with_highres(self):
         ERA5(
-            root_dir = '/data0/datasets/weatherbench/data/weatherbench/era5/5.625deg/',
-            root_highres_dir = '/data0/datasets/weatherbench/data/weatherbench/era5/2.8125deg/',
-            variables = ['2m_temperature', '10m_u_component_of_wind', '10m_v_component_of_wind'],
+            root_dir="/data0/datasets/weatherbench/data/weatherbench/era5/5.625deg/",
+            root_highres_dir="/data0/datasets/weatherbench/data/weatherbench/era5/2.8125deg/",
+            variables=[
+                "2m_temperature",
+                "10m_u_component_of_wind",
+                "10m_v_component_of_wind",
+            ],
             years=range(2000, 2014),
         )
--- tests/data/era5_downscaling_dataset_test.py
+++ tests/data/era5_downscaling_dataset_test.py
@@ -1,15 +1,16 @@
 from climate_learn.data.modules import ERA5Downscaling
 from climate_learn.utils.datetime import Hours
 
+
 class TestERA5DownscalingDatasetInstantiation:
     def test_era5_downscaling(self):
         ERA5Downscaling(
-            root_dir = '/data0/datasets/weatherbench/data/weatherbench/era5/5.625deg/',
-            root_highres_dir = '/data0/datasets/weatherbench/data/weatherbench/era5/2.8125deg/',
-            in_vars = ['2m_temperature', '10m_u_component_of_wind'],
-            out_vars = ['2m_temperature', '10m_v_component_of_wind'],
-            history = 1,
-            window = 6,
-            pred_range = (Hours(6)).hours(),
+            root_dir="/data0/datasets/weatherbench/data/weatherbench/era5/5.625deg/",
+            root_highres_dir="/data0/datasets/weatherbench/data/weatherbench/era5/2.8125deg/",
+            in_vars=["2m_temperature", "10m_u_component_of_wind"],
+            out_vars=["2m_temperature", "10m_v_component_of_wind"],
+            history=1,
+            window=6,
+            pred_range=(Hours(6)).hours(),
             years=range(2010, 2014),
         )
