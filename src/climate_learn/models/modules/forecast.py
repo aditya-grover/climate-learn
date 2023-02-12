9e5036d5af3180410fabf3af1341b91989146eea
Tung Nguyen <ductungnguyen1997@gmail.com>
2023-02-11 17:19:35 -0800

    Remove uncertainty quantification features (#60)
    * Remove uncertainty metrics
    * Remove uncertainty model components
    * Remove uncertainty-related code in forecast module
    * Remove redundant code
    * Formatting

--- src/climate_learn/models/modules/forecast.py
+++ src/climate_learn/models/modules/forecast.py
@@ -8,16 +8,7 @@ from torchvision.transforms import transforms
 from sklearn.linear_model import Ridge
 
 from .utils.lr_scheduler import LinearWarmupCosineAnnealingLR
-from .utils.metrics import (
-    crps_gaussian,
-    crps_gaussian_val,
-    lat_weighted_acc,
-    lat_weighted_mse,
-    lat_weighted_nll,
-    lat_weighted_rmse,
-    categorical_loss,
-    lat_weighted_spread_skill_ratio,
-)
+from .utils.metrics import lat_weighted_acc, lat_weighted_mse, lat_weighted_rmse
 
 
 class ForecastLitModule(LightningModule):
@@ -37,30 +28,8 @@ class ForecastLitModule(LightningModule):
         self.net = net
         self.test_loss = [lat_weighted_rmse, lat_weighted_acc]
         self.lr_baseline = None
-        if net.prob_type == "parametric":
-            self.train_loss = [crps_gaussian]
-            # self.train_loss = lat_weighted_nll
-            self.val_loss = [crps_gaussian_val, lat_weighted_spread_skill_ratio]
-        elif net.prob_type == "mcdropout":
-            self.train_loss = [lat_weighted_mse]
-            self.val_loss = [crps_gaussian_val]
-            raise NotImplementedError(
-                "Only parametric and deterministic prediction is supported"
-            )
-        elif net.prob_type == "categorical":
-            # loss functions need to be determined later (?)
-            self.train_loss = [categorical_loss]
-            self.val_loss = [categorical_loss]
-            self.test_loss = [categorical_loss]
-            self.num_bins = 50
-            self.bin_min = -5
-            self.bin_max = 5
-            self.bins = np.linspace(self.bin_min, self.bin_max, self.num_bins + 1)
-            self.bins[0] = -np.inf
-            self.bins[-1] = np.inf
-        else:  # deter
-            self.train_loss = [lat_weighted_mse]
-            self.val_loss = [lat_weighted_rmse]
+        self.train_loss = [lat_weighted_mse]
+        self.val_loss = [lat_weighted_rmse]
 
         if optimizer == "adam":
             self.optim_cls = torch.optim.Adam
@@ -75,12 +44,6 @@ class ForecastLitModule(LightningModule):
 
     def set_denormalization(self, mean, std):
         self.denormalization = transforms.Normalize(mean, std)
-
-        mean_mean_denorm, mean_std_denorm = -mean / std, 1 / std
-        self.mean_denormalize = transforms.Normalize(mean_mean_denorm, mean_std_denorm)
-
-        std_mean_denorm, std_std_denorm = np.zeros_like(std), 1 / std
-        self.std_denormalize = transforms.Normalize(std_mean_denorm, std_std_denorm)
 
     def set_lat_lon(self, lat, lon):
         self.lat = lat
@@ -100,19 +63,6 @@ class ForecastLitModule(LightningModule):
 
     def training_step(self, batch: Any, batch_idx: int):
         x, y, _, out_variables = batch
-
-        # transform y into one-hot format for categorical
-        # following the implemention on https://github.com/sagar-garg/WeatherBench/blob/f41f497ac45377d363dc30bfa77daf50d7b28afd/src/data_generator.py#L335
-        if self.net.prob_type == "categorical":
-            y_shape = y.shape  # [128, 1, 32, 64]
-            y = pd.cut(y.cpu().reshape(-1), self.bins, labels=False).reshape(y_shape)
-            # get one-hot encoded tensors. [128, 1, 32, 64, 50]
-            # equivalent to tf.keras.utils.to_categorical(y, num_classes=self.num_bins) in original implementation
-            y = np.eye(self.num_bins, dtype="float")[y]
-            y = y.reshape((*y_shape, self.num_bins))
-            y = torch.from_numpy(y).view(
-                y.shape[0], 50, 1, 32, 64
-            )  # [128, 1, 32, 64, 50]
 
         loss_dict, _ = self.net.forward(
             x, y, out_variables, metric=self.train_loss, lat=self.lat
@@ -141,22 +91,8 @@ class ForecastLitModule(LightningModule):
         ]
         steps = [int(s) for s in default_steps if s <= pred_steps and s > 0]
         days = [int(s * pred_range / 24) for s in steps]
-        day = int(days_each_step)
 
-        # transform y into one-hot format for categorical
-        # following the implemention on https://github.com/sagar-garg/WeatherBench/blob/f41f497ac45377d363dc30bfa77daf50d7b28afd/src/data_generator.py#L335
-        if self.net.prob_type == "categorical":
-            y_shape = y.shape  # [128, 1, 32, 64]
-            y = pd.cut(y.cpu().reshape(-1), self.bins, labels=False).reshape(y_shape)
-            # get one-hot encoded tensors. [128, 1, 32, 64, 50]
-            # equivalent to tf.keras.utils.to_categorical(y, num_classes=self.num_bins) in original implementation
-            y = np.eye(self.num_bins, dtype="float")[y]
-            y = y.reshape((*y_shape, self.num_bins))
-            y = torch.from_numpy(y).view(
-                y.shape[0], 50, 1, 32, 64
-            )  # [128, 1, 32, 64, 50]
-
-        all_loss_dicts, _ = self.net.val_rollout(
+        all_loss_dicts, _ = self.net.rollout(
             x,
             y,
             self.val_clim,
@@ -168,9 +104,6 @@ class ForecastLitModule(LightningModule):
             lat=self.lat,
             log_steps=steps,
             log_days=days,
-            mean_transform=self.mean_denormalize,
-            std_transform=self.std_denormalize,
-            log_day=day,
         )
         loss_dict = {}
         for d in all_loss_dicts:
@@ -193,7 +126,6 @@ class ForecastLitModule(LightningModule):
         x, y, variables, out_variables = batch
         pred_steps = y.shape[1]
         pred_range = self.pred_range.hours()
-        day = int(pred_range / 24)
 
         default_days = [1, 3, 5]
         days_each_step = pred_range / 24
@@ -202,101 +134,8 @@ class ForecastLitModule(LightningModule):
         ]
         steps = [int(s) for s in default_steps if s <= pred_steps and s > 0]
         days = [int(s * pred_range / 24) for s in steps]
-        day = int(days_each_step)
 
-        # rmse for climatology baseline
-        clim_pred = self.train_clim  # C, H, W
-        clim_pred = (
-            clim_pred.unsqueeze(0)
-            .unsqueeze(0)
-            .repeat(y.shape[0], y.shape[1], 1, 1, 1)
-            .to(y.device)
-        )
-        baseline_rmse = lat_weighted_rmse(
-            clim_pred,
-            y,
-            out_variables,
-            transform_pred=False,
-            transform=self.denormalization,
-            lat=self.lat,
-            log_steps=steps,
-            log_days=days,
-        )
-        for var in baseline_rmse.keys():
-            self.log(
-                "test_climatology_baseline/" + var,
-                baseline_rmse[var],
-                on_step=False,
-                on_epoch=True,
-                sync_dist=True,
-                batch_size=len(x),
-            )
-
-        # rmse for persistence baseline
-        pers_pred = x  # B, 1, C, H, W
-        baseline_rmse = lat_weighted_rmse(
-            pers_pred,
-            y,
-            out_variables,
-            transform_pred=True,
-            transform=self.denormalization,
-            lat=self.lat,
-            log_steps=steps,
-            log_days=days,
-        )
-        for var in baseline_rmse.keys():
-            self.log(
-                "test_persistence_baseline/" + var,
-                baseline_rmse[var],
-                on_step=False,
-                on_epoch=True,
-                sync_dist=True,
-                batch_size=len(x),
-            )
-
-        # rmse for linear regression baseline
-        # check if fit_lin_reg_baseline is called by checking whether self.lr_baseline is initialized
-        if self.lr_baseline:
-            lr_pred = self.lr_baseline.predict(
-                x.cpu().reshape((x.shape[0], -1))
-            ).reshape(y.shape)
-
-            lr_pred = lr_pred[:, np.newaxis, :, :, :]  # B, 1, C, H, W
-            lr_pred = torch.from_numpy(lr_pred).float().to(y.device)
-            baseline_rmse = lat_weighted_rmse(
-                lr_pred,
-                y,
-                out_variables,
-                transform_pred=True,
-                transform=self.denormalization,
-                lat=self.lat,
-                log_steps=steps,
-                log_days=days,
-            )
-            for var in baseline_rmse.keys():
-                self.log(
-                    "test_ridge_regression_baseline/" + var,
-                    baseline_rmse[var],
-                    on_step=False,
-                    on_epoch=True,
-                    sync_dist=True,
-                    batch_size=len(x),
-                )
-
-        # transform y into one-hot format for categorical
-        # following the implemention on https://github.com/sagar-garg/WeatherBench/blob/f41f497ac45377d363dc30bfa77daf50d7b28afd/src/data_generator.py#L335
-        if self.net.prob_type == "categorical":
-            y_shape = y.shape  # [128, 1, 32, 64]
-            y = pd.cut(y.cpu().reshape(-1), self.bins, labels=False).reshape(y_shape)
-            # get one-hot encoded tensors. [128, 1, 32, 64, 50]
-            # equivalent to tf.keras.utils.to_categorical(y, num_classes=self.num_bins) in original implementation
-            y = np.eye(self.num_bins, dtype="float")[y]
-            y = y.reshape((*y_shape, self.num_bins))
-            y = torch.from_numpy(y).view(
-                y.shape[0], 50, 1, 32, 64
-            )  # [128, 1, 32, 64, 50]
-
-        all_loss_dicts, _ = self.net.test_rollout(
+        all_loss_dicts, _ = self.net.rollout(
             x,
             y,
             self.test_clim,
@@ -308,9 +147,6 @@ class ForecastLitModule(LightningModule):
             lat=self.lat,
             log_steps=steps,
             log_days=days,
-            mean_transform=self.mean_denormalize,
-            std_transform=self.std_denormalize,
-            log_day=day,
         )
 
         loss_dict = {}
@@ -378,7 +214,8 @@ class ForecastLitModule(LightningModule):
                 batch_size=len(x),
             )
 
-        # rmse for linear regression baseline, if trained
+        # rmse for linear regression baseline
+        # check if fit_lin_reg_baseline is called by checking whether self.lr_baseline is initialized
         if self.lr_baseline:
             lr_pred = self.lr_baseline.predict(
                 x.cpu().reshape((x.shape[0], -1))
