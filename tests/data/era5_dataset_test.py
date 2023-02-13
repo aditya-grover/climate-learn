2e6a23761eb2bfa98459eb0244c4aba6634ee0fc
Prakhar Sharma <prakhar6sharma@gmail.com>
2023-02-13 14:32:40 -0800

    Skip data tests when running on github

--- tests/data/era5_dataset_test.py
+++ tests/data/era5_dataset_test.py
@@ -1,10 +1,17 @@
+import os
 from climate_learn.data.modules import ERA5
+import pytest
+
+DATA_PATH = "/data0/datasets/weatherbench/data/weatherbench/"
+
+GITHUB_ACTIONS = os.environ.get("GITHUB_ACTIONS") == "true"
 
 
+@pytest.mark.skipif(GITHUB_ACTIONS, reason="only works locally")
 class TestERA5DatasetInstantiation:
     def test_era5(self):
         ERA5(
-            root_dir="/data0/datasets/weatherbench/data/weatherbench/era5/5.625deg/",
+            root_dir=os.path.join(DATA_PATH, "era5/5.625deg/"),
             root_highres_dir=None,
             variables=[
                 "2m_temperature",
@@ -16,8 +23,8 @@ class TestERA5DatasetInstantiation:
 
     def test_era5_with_highres(self):
         ERA5(
-            root_dir="/data0/datasets/weatherbench/data/weatherbench/era5/5.625deg/",
-            root_highres_dir="/data0/datasets/weatherbench/data/weatherbench/era5/2.8125deg/",
+            root_dir=os.path.join(DATA_PATH, "era5/5.625deg/"),
+            root_highres_dir=os.path.join(DATA_PATH, "era5/2.8125deg/"),
             variables=[
                 "2m_temperature",
                 "10m_u_component_of_wind",
