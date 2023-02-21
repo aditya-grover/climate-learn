a7659c05083ee280e39a3add666d5ba8c23a60b7
se0ngbin <63076158+se0ngbin@users.noreply.github.com>
2023-02-21 09:11:58 -0800

    Add early stopping functionality for trainer module (#69)

--- src/climate_learn/training/train.py
+++ src/climate_learn/training/train.py
@@ -4,6 +4,7 @@ from pytorch_lightning.callbacks import (
     ModelCheckpoint,
     RichModelSummary,
     RichProgressBar,
+    EarlyStopping,
 )
 
 import logging
@@ -21,6 +22,9 @@ class Trainer:
         precision=16,
         max_epochs=4,
         logger=False,
+        patience=5,
+        early_stopping=False,
+        task=None,
     ):
         seed_everything(seed)
 
@@ -33,13 +37,35 @@ class Trainer:
         summary_callback = RichModelSummary(max_depth=-1)
         progress_callback = RichProgressBar()
 
+        callbacks = [
+            checkpoint_callback,
+            summary_callback,
+            progress_callback,
+        ]
+
+        if early_stopping:
+            if task == "forecasting":
+                monitor = "val/w_mse"
+            elif task == "downscaling":
+                monitor = "val/mse"
+            else:
+                raise NotImplementedError(
+                    "Please specify either forecasting or downscaling as the training task. Other tasks not available."
+                )
+
+            early_stop_callback = EarlyStopping(
+                monitor=monitor, patience=patience, verbose=False, mode="min"
+            )
+
+            callbacks.append(early_stop_callback)
+
         self.trainer = LitTrainer(
             logger=logger,
             accelerator=accelerator,
             devices=devices,
             precision=precision,
             max_epochs=max_epochs,
-            callbacks=[checkpoint_callback, summary_callback, progress_callback],
+            callbacks=callbacks,
         )
 
     def fit(self, model_module, data_module):
