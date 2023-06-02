b31ff20d01c557ac0aabc45fa53737c0af1e1b5f
Jason Jewik <jason.jewik@gmail.com>
2023-06-02 11:28:26 -0700

    ERA5 downscaling updates

--- scripts/table5-dl.py
+++ scripts/table5-dl.py
@@ -21,6 +21,7 @@ def main():
     parser.add_argument("preset")
     parser.add_argument("variable", choices=list(out_var_dict.keys()))
     parser.add_argument("gpu", type=int)    
+    parser.add_argument("--checkpoint", default=None)
     args = parser.parse_args()
     
     variables = [
@@ -69,19 +70,38 @@ def main():
         preset=args.preset
     )
     logger = TensorBoardLogger(save_dir=f"{default_root_dir}/logs")
-    trainer = cl.Trainer(
-        early_stopping="val/mse:aggregate",
-        patience=5,
-        accelerator="gpu",
-        devices=[args.gpu],
-        max_epochs=50,
-        default_root_dir=default_root_dir,
-        logger=logger,
-        precision="16",
-        summary_depth=1
-    )
-    trainer.fit(model, datamodule=dm)
-    trainer.test(model, datamodule=dm, ckpt_path="best")
+    if args.checkpoint:
+        trainer = cl.Trainer(
+            accelerator="gpu",
+            devices=[args.gpu],
+            precision="16",
+            summary_depth=1
+        )
+        model = cl.LitModule.load_from_checkpoint(
+            args.checkpoint,
+            net=model.net,
+            optimizer=model.optimizer,
+            lr_scheduler=None,
+            train_loss=None,
+            val_loss=None,
+            test_loss=model.test_loss,
+            test_target_tranfsorms=model.test_target_transforms
+        )
+        trainer.test(model, datamodule=dm)
+    else:
+        trainer = cl.Trainer(
+            early_stopping="val/mse:aggregate",
+            patience=5,
+            accelerator="gpu",
+            devices=[args.gpu],
+            max_epochs=50,
+            default_root_dir=default_root_dir,
+            logger=logger,
+            precision="16",
+            summary_depth=1
+        )
+        trainer.fit(model, datamodule=dm)
+        trainer.test(model, datamodule=dm, ckpt_path="best")
 
     
 if __name__ == "__main__":
