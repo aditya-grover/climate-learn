402afec2f62c602207562f9aa6e9a7d59c73ca84
Tung Nguyen <ductungnguyen1997@gmail.com>
2023-06-01 19:24:33 -0700

    add continuous data modules

--- src/climate_learn/data/__init__.py
+++ src/climate_learn/data/__init__.py
@@ -2,6 +2,7 @@ from .climate_dataset import *
 from .dataset import *
 from .download import *
 from .itermodule import *
+from .continuous_itermodule import *
 from .module import *
 from .nc2npz import convert_nc2npz
 from .task import *
